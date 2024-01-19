3b2fb64603229938c3f12052b4717bc46350f2d2
Andryss <andr.krivosheev@gmail.com>
2024-01-19 23:35:43 +0300

    Init project

---
+++ src/main/java/ru/andryss/nfsapi/NfsapiApplication.java
@@ -0,0 +1,13 @@
+package ru.andryss.nfsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfsapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NfsapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/andryss/nfsapi/NfsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andryss.nfsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfsapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
